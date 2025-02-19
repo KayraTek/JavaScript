422f44aa560914767839175c96bbaf6314208d46
KayraTek <35199453+KayraTek@users.noreply.github.com>
2025-02-19 19:15:45 +0300

    day4

---
+++ day-04/js/script-24.js
@@ -0,0 +1,14 @@
+function setMode(){
+    const modeControl = document.querySelector(`body`);
+    const setText = document.querySelector("#btn-mode");     
+
+    if(modeControl.classList.contains("light")){
+        modeControl.classList.remove("light");
+        modeControl.classList.add("dark");
+        setText.innerText = "Light";
+    }else{
+        modeControl.classList.remove("dark");
+        modeControl.classList.add("light");
+setText.innerText = "Dark";
+    }
+}
